6759050ab2bded2854f940863659dc46b51245e4
Marco Vignini <marco.vignini@stud.unifi.it>
2017-06-29 15:19:09 +0200

    Aggiunta attributo nome

--- myproject/src/myproject/Cestista.java
+++ myproject/src/myproject/Cestista.java
@@ -2,4 +2,21 @@ package myproject;
 
 public class Cestista {
 
+	private String nome;
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
