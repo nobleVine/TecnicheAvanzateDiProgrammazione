bf02e89e03472b1295c4e5d0af0dfba0b6deb21e
Lorenzo Marino <lorenzo.marino@stud.unifi.it>
2017-07-02 16:15:15 +0200

    Aggiunto Steph

--- myproject/src/myproject/Cestista.java
+++ myproject/src/myproject/Cestista.java
@@ -1,38 +1,6 @@
 package myproject;
 
 public class Cestista {
-<<<<<<< HEAD
-	
-	private int altezza;
-	private String squadra;
-	public String getSquadra() {
-		return squadra;
-	}
-	public void setSquadra(String squadra) {
-		this.squadra = squadra;
-	}
-	private int numScarpe;
-	private int salario;
-	
-	public int getNumScarpe() {
-		return numScarpe;
-	}
-	public void setNumScarpe(int numScarpe) {
-		this.numScarpe = numScarpe;
-	}
-	private int eta;
-	
-	public int getAltezza() {
-		return altezza;
-	}
-	public void setAltezza(int altezza) {
-		this.altezza = altezza;
-	}
-	
-	
-
-}
-=======
 
 	private String nome;
 	private String squadra;
@@ -96,6 +64,9 @@ public class Cestista {
 	public void getLebron() {
 		System.out.println("I'am only king");
 	}
+	
+	public void getSteph(){
+		System.out.println("Stepheeeeeeeeen Curryyyyyyyyyyyy");
+	}
 
 }
->>>>>>> refs/remotes/origin/master
