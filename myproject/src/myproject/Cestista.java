7e41dcca2a60c23b08ad9299f3c942578d4f1713
Marco Vignini <marco.vignini@stud.unifi.it>
2017-07-02 16:19:25 +0200

    fffffffff

--- myproject/src/myproject/Cestista.java
+++ myproject/src/myproject/Cestista.java
@@ -12,32 +12,18 @@ public class Cestista {
 		squadra = s;
 	}
 
-	/**
-	 * @return the nome
-	 */
 	public String getNome() {
 		return nome;
 	}
 
-	/**
-	 * @param nome
-	 *            the nome to set
-	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-	/**
-	 * @return the squadra
-	 */
 	public String getSquadra() {
 		return squadra;
 	}
 
-	/**
-	 * @param squadra
-	 *            the squadra to set
-	 */
 	public void setSquadra(String squadra) {
 		this.squadra = squadra;
 	}
@@ -46,17 +32,10 @@ public class Cestista {
 		System.out.println("LET'S GO");
 	}
 
-	/**
-	 * @return the freeAgency
-	 */
 	public boolean isFreeAgency() {
 		return freeAgency;
 	}
 
-	/**
-	 * @param freeAgency
-	 *            the freeAgency to set
-	 */
 	public void setFreeAgency(boolean freeAgency) {
 		this.freeAgency = freeAgency;
 	}
@@ -64,5 +43,7 @@ public class Cestista {
 	public void getLebron() {
 		System.out.println("I'am only king");
 	}
+	
+	
 
 }
